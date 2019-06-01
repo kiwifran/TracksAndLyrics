0d080c43aa258d970ca8679796c62cf53fa69606
FrankieWang <francescafranci@outlook.com>
2019-06-01 19:42:04 -0400

    more header styling

--- src/components/RellaxDisplay.js
+++ src/components/RellaxDisplay.js
@@ -3,11 +3,11 @@ function RellaxDisplay(){
     return(
         <div className="rellaxWrapper">
             <div  aria-hidden  className="musicSign musicSign1 rellax"  data-rellax-speed="1">♫</div>
-            <div  aria-hidden  className="musicSign musicSign2 rellax"  data-rellax-speed="3">♪</div>
+            <div aria-hidden className="musicSign musicSign2 rellax" data-rellax-speed="3">♩</div>
             <div  aria-hidden  className="musicSign musicSign3 rellax"  data-rellax-speed="3">♬</div>
-            <div  aria-hidden  className="musicSign musicSign4 rellax"  data-rellax-speed="6">♩</div>
+            <div aria-hidden className="musicSign musicSign4 rellax" data-rellax-speed="6">♪</div>
             <div  aria-hidden  className="musicSign musicSign5 rellax"  data-rellax-speed="1">♪</div>
-            <div  aria-hidden  className="musicSign musicSign6 rellax"  data-rellax-speed="2">♬</div>
+            <div aria-hidden className="musicSign musicSign6 rellax" data-rellax-speed="2.5">♫</div>
         </div>
     )
 }
