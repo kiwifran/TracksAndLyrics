39ecff4bb9883990520a6a06a26daf45554c7b18
FrankieWang <francescafranci@outlook.com>
2019-06-02 22:24:45 -0400

    Updates

--- precache-manifest.e5c99261cbfb749e92035f6a4594f46f.js
+++ precache-manifest.e5c99261cbfb749e92035f6a4594f46f.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e87666f15c28c56076de42718a4b73d0",
+    "revision": "59913773b98f8611b147c7c4e3e32863",
     "url": "/TracksAndLyrics/index.html"
   },
   {
-    "revision": "9aca3253c7bad1e39fed",
+    "revision": "3a63488c4286ecdfbb9f",
     "url": "/TracksAndLyrics/static/css/main.6c9c1329.chunk.css"
   },
   {
-    "revision": "2245c2a58a635faed732",
-    "url": "/TracksAndLyrics/static/js/2.463756ee.chunk.js"
+    "revision": "6770f935be3054a791d7",
+    "url": "/TracksAndLyrics/static/js/2.360e12cc.chunk.js"
   },
   {
-    "revision": "9aca3253c7bad1e39fed",
-    "url": "/TracksAndLyrics/static/js/main.2157b50f.chunk.js"
+    "revision": "3a63488c4286ecdfbb9f",
+    "url": "/TracksAndLyrics/static/js/main.63060b51.chunk.js"
   },
   {
     "revision": "2a1974e10e11394f4fc9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TracksAndLyrics/precache-manifest.e1818567cd07074e53d1699a1c30079e.js"
+  "/TracksAndLyrics/precache-manifest.e5c99261cbfb749e92035f6a4594f46f.js"
 );
 
 self.addEventListener('message', (event) => {
